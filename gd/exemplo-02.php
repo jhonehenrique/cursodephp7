bd3325bc60ee584b58d7d92474695c90d213ba5d
Jhone Oliver <jhone@oliverdigital.com.br>
2017-11-13 13:42:08 -0200

    Biblioteca GD, na Oliver

---
+++ gd/exemplo-02.php
@@ -0,0 +1,16 @@
+<?php
+
+$image = imagecreatefromjpeg("certificado.jpg");
+
+$titleColor = imagecolorallocate($image, 0, 0, 0);
+$gray = imagecolorallocate($image, 100, 100, 100);
+
+imagestring($image, 5, 450, 150, "CERTIFICADO", $titleColor);
+imagestring($image, 5, 440, 350, "Jhone Henrique", $titleColor);
+imagestring($image, 3, 440, 370, utf8_encode("Concluido em: ") .date("d/m/Y") , $titleColor);
+header("Content-type: image/jpeg");
+
+imagejpeg($image);
+imagedestroy($image);
+
+?>
---
+++ zestudos/index.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once("config.php");
+
+// $sql = new Sql();
+
+// $usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+// echo json_encode($usuarios);
+
+
+
+//Carrega um usuario por ID//
+// $root = new Usuario();
+// $root->loadById(3);
+
+
+
+//Carrega uma lista de usuarios//
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+
+//Carrega lista de usuario buscando pelo login//
+//$search = Usuario::search("jhone");
+//echo json_encode($search);
+
+//Carega usuario usando login e senha//
+// $usuario = new Usuario();
+// $usuario->login("Jhone", "abcd123");
+// echo $usuario;
+
+
+
+
+// $aluno = new Usuario();
+// $aluno->setDeslogin("Jhone");
+// $aluno->setDessenha("0101");
+//$aluno = new Usuario();
+//$aluno->setDeslogin("Aluno");
+//$aluno->setDessenha("lin0");
+
+ $aluno = new Usuario("Develper", "webphp2");
+ $aluno->insert();
+ echo $aluno;
+
+
+
+//UPDATE//
+// $usuario = new Usuario();
+// $usuario->loadById(25);
+// $usuario->update("Programador", "phpwebdeveloper");
+// echo $usuario;
+
+
+//DELETE//
+// $usuario = new Usuario();
+// $usuario->loadById(25);
+// $usuario->delete();
+// echo $usuario;
+
+
+
+?>
