fd02b3aec582cdfbf4b0c47341da1ecd8c781a23
Jhone Henrique <jhone.henrique.info@gmail.com>
2017-11-14 00:26:19 -0200

    Gerando Boletos, em casa

--- gd/exemplo-02.php
+++ gd/exemplo-02.php
@@ -5,12 +5,12 @@ $image = imagecreatefromjpeg("certificado.jpg");
 $titleColor = imagecolorallocate($image, 0, 0, 0);
 $gray = imagecolorallocate($image, 100, 100, 100);
 
-imagestring($image, 5, 450, 150, "CERTIFICADO", $titleColor);
+imagestring($image, 10, 450, 150, "CERTIFICADO", $titleColor);
 imagestring($image, 5, 440, 350, "Jhone Henrique", $titleColor);
 imagestring($image, 3, 440, 370, utf8_encode("Concluido em: ") .date("d/m/Y") , $titleColor);
 header("Content-type: image/jpeg");
 
-imagejpeg($image);
+imagejpeg($image, "certificado-".date("Y-m-d").".jpg", 100);
 imagedestroy($image);
 
 ?>
