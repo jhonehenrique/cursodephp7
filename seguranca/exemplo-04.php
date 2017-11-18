0c5b04bef2bbe6a2c33c21b78bbdf36b0857ee05
Jhone Oliver <jhone@oliverdigital.com.br>
2017-11-18 17:46:59 -0200

    Ainda no recaptcha na Oliver

--- seguranca/exemplo-04.php
+++ seguranca/exemplo-04.php
@@ -1,9 +1,7 @@
-<?php
-
-
-?>
+<script src='https://www.google.com/recaptcha/api.js'></script>
 
 <form action="cadastrar.php" method="post">
+	<div class="g-recaptcha" data-sitekey="6LeTNTkUAAAAALN9X3TlnXU4XkHK_SG4JY9ALhHQ"></div>
 	<input type="email" name="inputEmail">
 	<button type="submit">Enviar</button>
 </form>
